52ed677351b1540dcda026a308ecb8a0c05f4ee0
Rokyl <Shidlovskiy-02@mail.ru>
2021-10-01 14:28:28 +0300

    task32 Pandigital Products

--- task32.rb
+++ task32.rb
@@ -1,1 +1,20 @@
+products_arr = []
+arr = (1..9).to_a
+arr_m = []
 
+(1..9999).each do |multiplicand|
+  (1..9999).each do |multiplier|
+    arr_m.clear
+    product = multiplicand * multiplier
+
+    arr_m << multiplicand.digits
+    arr_m << multiplier.digits
+    arr_m << product.digits
+    arr_m.flatten! # двумерка в одномерку
+
+    break if (arr_m.size > 10)
+    products_arr << product if (arr_m.uniq.sort == arr && arr_m.uniq.size == arr_m.size)
+  end
+end
+
+p products_arr.uniq.sum # uniq для уникальности элементов, перемножающихся дважды
