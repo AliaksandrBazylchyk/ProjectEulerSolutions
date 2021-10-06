fa29349e4b01952fab023c1455c6d06cabd57f8c
Rokyl <Shidlovskiy-02@mail.ru>
2021-10-06 17:07:23 +0300

    task65 E-Solution

--- task65.rb
+++ task65.rb
@@ -1,1 +1,14 @@
+def fr(n)
+  if n % 3 == 0
+    return n / 3 * 2
+  else
+    return 1
+  end
+end
 
+e = 0
+100.downto(2) do |n|
+  e = Rational('1') / (fr(n) + e)
+end
+e += 2
+puts e.numerator.to_s.each_char.map(&:to_i).inject(:+)
